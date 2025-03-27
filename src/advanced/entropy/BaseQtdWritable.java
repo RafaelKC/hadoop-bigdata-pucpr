698f4dcf6f066ff83dcb78134be03489a77c5bbe
RafaelKC <rafaelkaua97@gmail.com>
2025-03-27 20:23:28 -0300

    feito

--- src/advanced/entropy/BaseQtdWritable.java
+++ src/advanced/entropy/BaseQtdWritable.java
@@ -9,6 +9,43 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.regex.Pattern;
 
-public class BaseQtdWritable {
+public class BaseQtdWritable implements Writable {
+    private String base;
+    private long qtd;
 
+    public BaseQtdWritable() {
+    }
+
+    public BaseQtdWritable(String base, long qtd) {
+        this.base = base;
+        this.qtd = qtd;
+    }
+
+    public long getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(long qtd) {
+        this.qtd = qtd;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeUTF(base);
+        dataOutput.writeUTF(String.valueOf(qtd));
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        base = dataInput.readUTF();
+        qtd = Long.parseLong(dataInput.readUTF());
+    }
 }
--- src/advanced/entropy/EntropyFASTA.java
+++ src/advanced/entropy/EntropyFASTA.java
@@ -1,5 +1,6 @@
 package advanced.entropy;
 
+import basic.Teste;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
@@ -14,7 +15,9 @@ import org.apache.log4j.BasicConfigurator;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class EntropyFASTA {
 
@@ -22,26 +25,74 @@ public class EntropyFASTA {
         BasicConfigurator.configure();
 
         Configuration c = new Configuration();
-        String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
         // arquivo de entrada
-        Path input = new Path(files[0]);
+        Path input = new Path("in/JY157487.1.fasta");
 
         Path intermediate = new Path("./output/intermediate.tmp");
 
         // arquivo de saida
-        Path output = new Path(files[1]);
+        Path output = new Path("output/entropia.txt");
+
+
+        Job job1 = new Job(c, "etapa A");
+        Job job2 = new Job(c, "etapa B");
+
+        job1.setJarByClass(EntropyFASTA.class);
+        job1.setMapperClass(MapEtapaA.class);
+        job1.setReducerClass(ReduceEtapaA.class);
+
+        job1.setMapOutputKeyClass(Text.class);
+        job1.setMapOutputValueClass(LongWritable.class);
+
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(LongWritable.class);
+
+        FileInputFormat.addInputPath(job1, input);
+        FileOutputFormat.setOutputPath(job1, intermediate);
+
+        job1.waitForCompletion(true);
+
+        job2.setJarByClass(EntropyFASTA.class);
+        job2.setMapperClass(MapEtapaB.class);
+        job2.setReducerClass(ReduceEtapaB.class);
+
+        job2.setMapOutputKeyClass(Text.class);
+        job2.setMapOutputValueClass(BaseQtdWritable.class);
+
+        job2.setOutputKeyClass(Text.class);
+        job2.setOutputValueClass(DoubleWritable.class);
+
+        FileInputFormat.addInputPath(job2, intermediate);
+        FileOutputFormat.setOutputPath(job2, output);
+
+        job2.waitForCompletion(true);
 
     }
 
     public static class MapEtapaA extends Mapper<LongWritable, Text, Text, LongWritable> {
         public void map(LongWritable key, Text value, Context con)
                 throws IOException, InterruptedException {
+            String linha = value.toString();
+            if (linha.startsWith(">gi")) return;
+            String[] c = linha.split("");
+
+            for(String l: c) {
+                con.write(new Text(l), new LongWritable(1));
+            }
+
+            con.write(new Text("count"), new LongWritable(c.length));
         }
     }
 
     public static class ReduceEtapaA extends Reducer<Text, LongWritable, Text, LongWritable> {
         public void reduce(Text key, Iterable<LongWritable> values, Context con)
                 throws IOException, InterruptedException {
+            long soma = 0;
+            for (LongWritable l: values) {
+                soma += l.get();
+            }
+
+            con.write(key, new LongWritable(soma));
 
         }
     }
@@ -50,6 +101,10 @@ public class EntropyFASTA {
     public static class MapEtapaB extends Mapper<LongWritable, Text, Text, BaseQtdWritable> {
         public void map(LongWritable key, Text value, Context con)
                 throws IOException, InterruptedException {
+            String linha = value.toString();
+            String[] c = linha.split("\t");
+
+            con.write(new Text("global"), new BaseQtdWritable(c[0], Long.parseLong(c[1])));
 
         }
     }
@@ -57,6 +112,24 @@ public class EntropyFASTA {
     public static class ReduceEtapaB extends Reducer<Text, BaseQtdWritable, Text, DoubleWritable> {
         public void reduce(Text key, Iterable<BaseQtdWritable> values, Context con)
                 throws IOException, InterruptedException {
+            long count = 0;
+
+            for(BaseQtdWritable base: values) {
+                if (base.getBase().equals("count")) {
+                    count = base.getQtd();
+                    break;
+                }
+            }
+
+            for(BaseQtdWritable base: values) {
+                if (base.getBase().equals("count")) continue;
+                double valorC = base.getQtd();
+                double prop = valorC / (double) count;
+                double entropia = -prop * ( Math.log10(prop)/Math.log10(2) );
+
+                con.write(new Text(base.getBase()), new DoubleWritable(entropia));
+            }
+
         }
     }
 }
